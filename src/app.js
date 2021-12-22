31c10182ad30ec7f3214c8a717a01a7f4dfc8eb5
Eerkz <wcalemios@gmail.com>
2021-12-22 23:10:15 +0800

    added responsiveness, added nav features, added custom pointer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,22 @@ const closeIcon = document.querySelector('.close-icon')
 const wideMenuLinks = document.querySelector('.wide-menu')
 const mobileMenuLinks = document.querySelector('.mobile-menu-links')
 const scrollBtn = document.getElementById("scrollBtn");
+const heroSection = document.querySelector('.hero-section')
+const navbar = document.getElementById('nav-bar');
+
+
+init_pointer();
+
+init_pointer({
+  pointerColor: "#FFDE5B"
+})
+
+init_pointer({
+  pointerColor: "#FFDE5B",
+  ringSize: 15,
+  ringClickSize: 10
+})
+
 
 hamburger.addEventListener('click', ()=> {
     mobileMenu.style.display = "inline";
@@ -28,3 +44,30 @@ const topFunction = () => {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+var lastScrollTop;
+console.log(window.scrollY)
+
+window.scroll({
+  behavior: 'smooth'
+});
+
+
+window.addEventListener('scroll',function(){
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if(scrollTop > lastScrollTop){
+    navbar.style.top='-60px';
+    heroSection.style.cssText = 'margin-top:0px;';
+  }
+  else if(window.scrollY ===0){
+    navbar.style.cssText="background-color: transparent;"
+    heroSection.style.cssText = 'margin-top:80px;';
+  }
+  else {
+    navbar.style.cssText="background-color: #202022;"
+    heroSection.style.cssText = 'margin-top:80px;';
+    navbar.style.top='0';
+  }
+  lastScrollTop = scrollTop;
+});
